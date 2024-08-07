c62e5928168a71adb9bf69e2ae23e5c0d5beb3b5
jaycode <teguhwpurwanto@gmail.com>
2024-08-07 13:45:14 +0700

    Update models.py to use BlobServiceClient and set credentials in config.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -2,13 +2,14 @@ from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from azure.storage.blob import BlockBlobService
+from azure.storage.blob import BlobServiceClient
 import string, random
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 from flask import flash
 
 blob_container = app.config['BLOB_CONTAINER']
-blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
+blob_service_client = BlobServiceClient.from_connection_string(app.config['BLOB_CONNECTION_STRING'])
+container_client = blob_service_client.get_container_client(blob_container)
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -52,17 +53,19 @@ class Post(db.Model):
         self.user_id = userId
 
         if file:
-            filename = secure_filename(file.filename);
-            fileextension = filename.rsplit('.',1)[1];
-            Randomfilename = id_generator();
-            filename = Randomfilename + '.' + fileextension;
+            filename = secure_filename(file.filename)
+            fileextension = filename.rsplit('.', 1)[1]
+            Randomfilename = id_generator()
+            filename = Randomfilename + '.' + fileextension
             try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
-                if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
-            except Exception:
-                flash(Exception)
-            self.image_path =  filename
+                blob_client = container_client.get_blob_client(filename)
+                blob_client.upload_blob(file, overwrite=True)
+                if self.image_path:
+                    old_blob_client = container_client.get_blob_client(self.image_path)
+                    old_blob_client.delete_blob()
+            except Exception as e:
+                flash(str(e))
+            self.image_path = filename
         if new:
             db.session.add(self)
         db.session.commit()
